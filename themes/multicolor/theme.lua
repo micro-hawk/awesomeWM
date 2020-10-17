716cb71b7f2adc0192c90e6fd5af5587313c6ead
micro-hawk <dasvikas0071@gmail.com>
2020-10-17 09:49:33 +0530

    Updated Layout

--- themes/multicolor/theme.lua
+++ themes/multicolor/theme.lua
@@ -15,7 +15,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
-theme.wallpaper                                 = theme.confdir .. "/wall.jpg"
+theme.wallpaper                                 = theme.confdir .. "/wallP.png"
 theme.font                                      = "Noto Sans Regular 11"
 theme.taglist_font                              = "Noto Sans Regular 13"
 theme.menu_bg_normal                            = "#000000"
