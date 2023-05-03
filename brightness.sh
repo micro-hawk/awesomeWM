87ed0c4b09783d231d4b6abb60da2a89da818dfc
micro-hawk <dasvikas0071@gmail.com>
2023-05-04 01:21:13 +0530

    brightness script updated

--- brightness.sh
+++ brightness.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+dir_contents=$(ls /sys/class/backlight/)
+max_brightness=$(cat /sys/class/backlight/${dir_contents}/max_brightness)
+echo "Max Brightness: ${max_brightness}"
 
-echo "Max Brightness: 937"
+echo -n "Enter brightness %: "
+read brightness_percentage
 
-echo -n "Enter brightness: "
-read brightness
-
-echo "$brightness" > /sys/class/backlight/intel_backlight/brightness
+brightness=$(($brightness_percentage*$max_brightness/100));
+# echo $brightness
+if [ $brightness -le $max_brightness ]; then
+    echo "$brightness" > /sys/class/backlight/intel_backlight/brightness
+else
+    echo "Invalid brightness %"
+fi
